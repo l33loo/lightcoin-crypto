1eb15bd31490e40414b4438b842429d906fc117b
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-12 20:29:57 +0000

    remove dead code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
   }
 }
 
 class Transaction {
-  constructor(account, type, amount) {
+  constructor(account, amount) {
     this.account = account;
     this.amount = amount;
   }
@@ -32,14 +32,14 @@ class Withdrawal extends Transaction {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 const myAccount = new Account("snow-patrol");
-t1 = new Withdrawal(myAccount, "withdrawal", 50.25);
+t1 = new Withdrawal(myAccount, 50.25);
 t1.commit();
 console.log('Transaction 1:', t1.value);
 
-t2 = new Withdrawal(myAccount, "withdrawal", 9.99);
+t2 = new Withdrawal(myAccount, 9.99);
 t2.commit;
 console.log('Transaction 2:', t2.value);
-t3 = new Deposit(myAccount, "deposit", 134.56);
+t3 = new Deposit(myAccount, 134.56);
 t3.commit;
 console.log("Transaction 3: ", t3.value);
 
