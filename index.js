933293d06fad89fc376b837874ae109a967c75ba
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-12 20:24:05 +0000

    refactor commit

--- index.js
+++ index.js
@@ -6,39 +6,41 @@ class Account {
 }
 
 class Transaction {
-  constructor(account, amount) {
+  constructor(account, type, amount) {
     this.account = account;
     this.amount = amount;
   }
-}
-class Withdrawal extends Transaction {
 
   commit() {
-    this.account.balance -= this.amount;
+    this.account.balance += this.value;
   }
 }
 
 class Deposit extends Transaction {
-
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 const myAccount = new Account("snow-patrol");
-t1 = new Withdrawal(myAccount, 50.25);
+t1 = new Withdrawal(myAccount, "withdrawal", 50.25);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Transaction 1:', t1.value);
 
-t2 = new Withdrawal(myAccount, 9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-t3 = new Deposit(myAccount, 134.56);
-t3.commit();
-console.log("Transaction 3: ", t3);
+t2 = new Withdrawal(myAccount, "withdrawal", 9.99);
+t2.commit;
+console.log('Transaction 2:', t2.value);
+t3 = new Deposit(myAccount, "deposit", 134.56);
+t3.commit;
+console.log("Transaction 3: ", t3.value);
 
 console.log('Balance:', myAccount.balance);
